99f9cbd5f3921223149c665657834844ba73f0b1
WonderfulDonutSnack <thisurakawshal@gmail.com>
2020-10-28 13:54:24 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+import discord
+import random
+from discord.ext import commands, tasks
+from random import choice
+
+client = commands.Bot(command_prefix='*')
+
+status = ['hunting', 'cooking', 'eating']
+queue = []
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print('The bot is Online')
+
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))    
+
+
+client.run('NzcwMDkxMjc5MTUyNzA5NjQy.X5YhZA.dxNFWtR16pFYZyuD5IiqxgedjdY')    
